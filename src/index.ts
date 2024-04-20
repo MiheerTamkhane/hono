e0e5809717ce7d0e13261505b2a420ce24c33833
MiheerTamkhane <sarangtamkhane10@gmail.com>
2024-04-20 12:36:55 +0530

    updated the file structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
 
-export default app
+  return c.text("Hello Hono!");
+});
+
+// app.post("/", async (c) => {
+//   console.log("auth : ", c.req.header("Authorization"));
+//   console.log("auth : ", c.req.query("param"));
+//   return c.text("hello Hono!");
+// });
+
+export default app;
